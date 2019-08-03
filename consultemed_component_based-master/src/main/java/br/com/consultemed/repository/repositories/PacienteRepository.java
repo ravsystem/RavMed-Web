5ef9775dc9dab3bf5483aa12c61d13aff8fee0c1
ygohamon <igohamonjogos@gmail.com>
2019-08-03 13:33:25 -0300

    V0.5
    Foi criado cadastro da paciente

---
+++ consultemed_component_based-master/src/main/java/br/com/consultemed/repository/repositories/PacienteRepository.java
@@ -0,0 +1,82 @@
+package br.com.consultemed.repository.repositories;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import br.com.consultemed.models.Paciente;
+import br.com.consultemed.utils.JPAUtils;
+
+public class PacienteRepository {
+	
+	EntityManagerFactory emf = JPAUtils.getEntityManagerFactory();
+	EntityManager factory = emf.createEntityManager();
+
+	public List<Paciente> listaPacientes() {
+		Query query = this.factory.createQuery("SELECT object(p) FROM Pacinte as p");
+		return query.getResultList();
+	}
+
+	public Collection<Paciente> listarPacientes() throws Exception {
+		this.factory = emf.createEntityManager();
+		List<Paciente> contatos = new ArrayList<Paciente>();
+		try {
+			factory.getTransaction().begin();
+			TypedQuery<Paciente> query = factory.createNamedQuery("Paciente.findAll", Paciente.class);
+			contatos = query.getResultList();
+			factory.getTransaction().commit();
+
+		} catch (Exception e) {
+			e.getMessage();
+			this.factory.getTransaction().rollback();
+		} finally {
+			factory.close();
+		}
+
+		return contatos;
+	}
+
+	public void salvarPaciente(Paciente paciente) {
+		this.factory = emf.createEntityManager();
+		try {
+			factory.getTransaction().begin();
+			if (paciente.getId() == null) {
+				factory.persist(paciente);
+			} else {
+				factory.merge(paciente);
+			}
+			factory.getTransaction().commit();
+		} catch (Exception e) {
+			e.getMessage();
+			this.factory.getTransaction().rollback();
+
+		} finally {
+			factory.close();
+		}
+	}
+
+	public void deleteById(Long id) throws Exception {
+		this.factory = emf.createEntityManager();
+		Paciente paciente = new Paciente();
+
+		try {
+
+			paciente = factory.find(Paciente.class, id);
+			factory.getTransaction().begin();
+			factory.remove(paciente);
+			factory.getTransaction().commit();
+
+		} catch (Exception e) {
+			e.getMessage();
+			this.factory.getTransaction().rollback();
+		} finally {
+			factory.close();
+		}
+
+	}
+}
